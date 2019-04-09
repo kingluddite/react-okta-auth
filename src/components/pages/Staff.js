d64161a890502e5aeaf2e71f358293ec1d52c17b
kindluddite <howley.phil@gmail.com>
2019-04-08 23:19:48 -0700

    Fix staff link to box

--- src/components/pages/Staff.js
+++ src/components/pages/Staff.js
@@ -22,7 +22,7 @@ class Staff extends Component {
         <h1>Welcome {currentUserName}</h1>
         <p>Email: {currentUserEmail}</p>
         <p>You have reached the authorized staff area of the portal</p>
-        <a href="https://dev-414986.oktapreview.com/home/boxnet/0oaj0xvxefgJZ5O2b0h7/37" target="_blank">Box</a>
+        <a rel="noopener noreferrer" href="https://dev-414986.oktapreview.com/home/boxnet/0oaj0xvxefgJZ5O2b0h7/37" target="_blank">Box</a>
       </div>
     );
   }
